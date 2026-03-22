f35fae9166ba14655313ce562d3efcabdc1cb915
Cesar Castro <cesarrqc@gmail.com>
2026-03-22 15:20:43 -0300

    feat[config]: Commit inicial do projeto

---
+++ src/main/java/br/com/kodasoft/telegram/TelegramIntegrationSpringBootStarterStarterApplication.java
@@ -0,0 +1,13 @@
+package br.com.kodasoft.telegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramIntegrationSpringBootStarterStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramIntegrationSpringBootStarterStarterApplication.class, args);
+	}
+
+}
